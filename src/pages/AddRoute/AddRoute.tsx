53a129927da810a19bb9cfaa964d9dd9be8fbb8e
NLSteveO <steveokeefe88@gmail.com>
2023-09-06 20:41:04 -0230

    Update AddRoute page to take route details
    Route details collected using text input components.
    Submit button logs details to console.

--- src/pages/AddRoute/AddRoute.tsx
+++ src/pages/AddRoute/AddRoute.tsx
@@ -1,7 +1,82 @@
+import { useState } from 'react';
+
+interface Route {
+  id: string;
+  gym: string;
+  wall: string;
+  color: string;
+  grade: string;
+  dateSet: string;
+}
+
 function AddRoute() {
+  const [gym, setGym] = useState('');
+  const [wall, setWall] = useState('');
+  const [color, setColor] = useState('');
+  const [grade, setGrade] = useState('');
+  const [dateSet, setDateSet] = useState('');
+
+  const createRouteId = () => {
+    return Math.random().toString().substr(2, 9);
+  }
+
+  const gymOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return setGym(event.target.value);
+  };
+
+  const wallOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return setWall(event.target.value);
+  };
+
+  const colorOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return setColor(event.target.value);
+  };
+
+  const gradeOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return setGrade(event.target.value);
+  };
+
+  const dateSetOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return setDateSet(event.target.value);
+  };
+
+  const onClick = () => {
+    const Route: Route = {
+      id: createRouteId(),
+      gym,
+      wall,
+      color,
+      grade,
+      dateSet
+    };
+    console.log(JSON.stringify(Route));
+  };
+
   return (
     <>
-      <h1>HI!</h1>
+      <h1>Add Route</h1>
+      <label>
+        Gym: <input value={gym} onChange={gymOnChange} />
+      </label>
+      <br />
+      <label>
+        Wall: <input value={wall} onChange={wallOnChange} />
+      </label>
+      <br />
+      <label>
+        Route Color(Tape or Holds): <input value={color} onChange={colorOnChange} />
+      </label>
+      <br />
+      <label>
+        Grade: <input value={grade} onChange={gradeOnChange} />
+      </label>
+      <br />
+      <label>
+        Date Set: <input value={dateSet} onChange={dateSetOnChange} />
+      </label>
+      <br />
+      <br />
+      <button onClick={onClick}>Submit</button>
     </>
   );
 }
--- tests/src/pages/AddRoute/AddRoute.test.tsx
+++ tests/src/pages/AddRoute/AddRoute.test.tsx
@@ -1,9 +1,37 @@
-import { describe, expect, it } from 'vitest';
+import React from 'react';
+import { describe, expect, it, vi } from 'vitest';
+import { renderHook, render, screen, fireEvent } from '@testing-library/react';
 import { AddRoute } from '../../../../src/pages';
 
+const consoleSpy = vi.spyOn(console, 'log');
+vi.spyOn(Math, 'random').mockReturnValue(0.123456789);
+
 describe('AddRoute', () => {
-  it('should render the page correctly', () => {
-    const result = AddRoute();
-    expect(result).toMatchSnapshot();
+  describe('Snapshots', () => {
+    it('should render the page correctly', () => {
+      const { result } = renderHook(AddRoute);
+      expect(result.current).toMatchSnapshot();
+    });
+  });
+
+  describe('onClick', () => {
+    it('should log the route to the console', () => {
+      render(<AddRoute />);
+      const button = screen.getByRole('button');
+      fireEvent.change(screen.getByLabelText('Gym:'), { target: { value: 'The Cove' } });
+      fireEvent.change(screen.getByLabelText('Wall:'), { target: { value: 'Chossy Cove' } });
+      fireEvent.change(screen.getByLabelText('Route Color(Tape or Holds):'), { target: { value: 'Black Holds' } });
+      fireEvent.change(screen.getByLabelText('Grade:'), { target: { value: 'Green Circle' } });
+      fireEvent.change(screen.getByLabelText('Date Set:'), { target: { value: '2023-09-06' } });
+      fireEvent.click(button);
+      expect(consoleSpy).toHaveBeenCalledWith(JSON.stringify({
+        id: '123456789',
+        gym: 'The Cove',
+        wall: 'Chossy Cove',
+        color: 'Black Holds',
+        grade: 'Green Circle',
+        dateSet: '2023-09-06'
+      }));
+    });
   });
 });
