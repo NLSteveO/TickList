13f1de133cf1cecb852bf8c663df780560d1528a
Steve O <steve@getmysa.com>
2023-12-26 21:12:35 -0330

    WIP: try and use Nx

--- src/__tests__/pages/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-import Home from '../../pages/Home';
-
-describe('Home', () => {
-  it('should render the page correctly', () => {
-    const result = Home();
-    expect(result).toMatchSnapshot();
-  });
-});
