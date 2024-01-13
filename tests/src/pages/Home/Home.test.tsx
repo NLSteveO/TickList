8b6bb9b602121bed5fe211fb9574559a99d71724
Steve O <steve@getmysa.com>
2024-01-12 22:44:32 -0330

    Restructure tests directory to match repo src code

--- tests/src/pages/Home/Home.test.tsx
+++ tests/src/pages/Home/Home.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import Home from '../../src/pages/Home/Home';
+import Home from '../../../../src/pages/Home/Home';
 
 describe('Home', () => {
   it('should render the page correctly', () => {
